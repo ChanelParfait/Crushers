b2fd9cf93fe631497d4f2937e2ef17b449ddbac1
ARARECREATURE <Anthony.thai@student.uts.edu.au>
2024-09-04 12:31:34 +1000

    Rocket/Speed implementation

--- Crushers/Assets/Rocket.cs
+++ Crushers/Assets/Rocket.cs
@@ -2,24 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rB;
-    [SerializeField] private float _Speed = 10f;
+    [SerializeField] private float _Speed = 1000f;
 
-    private void Awake()
+    
+    private void Start()
     {
-        
+        this.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * _Speed, ForceMode.Force);
     }
 
-    private void Update()
+    private void OnCollisionEnter(Collision other)
     {
-        rB.AddForce(transform.forward * _Speed, ForceMode.Force);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
+        ExplosionDamage(this.transform.position, 20f);
         Destroy(this.gameObject);
+    }
+
+    private void ExplosionDamage(Vector3 postion, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(postion, radius);
+        foreach (var hitcollider in hitColliders)
+        {
+            if (hitcollider.transform.root.gameObject.CompareTag("Player"))
+            {
+                Debug.Log(hitcollider.transform.root.gameObject);
+                hitcollider.transform.root.GetComponent<Rigidbody>().AddForce(hitcollider.transform.root.up  * 500000f, ForceMode.Force);
+                
+            }
+        }
     }
 }
