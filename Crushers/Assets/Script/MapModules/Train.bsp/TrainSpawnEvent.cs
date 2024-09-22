a4804356b2fbc54317c25c10a669419e20453472
DoMess <michael.lam14@outlook.com>
2024-09-22 20:13:30 +1000

    [GM-55] Update 0.6
    Added barrels
    Removed old scripts
    Created train trap
    WIP: Spawning trains

---
+++ Crushers/Assets/Script/MapModules/Train.bsp/TrainSpawnEvent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainSpawnEvent : MonoBehaviour
+{
+    [SerializeField] GameObject trainPrefab;
+    [SerializeField] GameObject spawnPoint;
+
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
