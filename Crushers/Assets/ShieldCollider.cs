8939e3be1d092265edcc6e7c2b71e6353a4578a0
ARARECREATURE <Anthony.thai@student.uts.edu.au>
2024-09-22 20:39:25 +1000

    Updating PickUps

---
+++ Crushers/Assets/ShieldCollider.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldCollider : MonoBehaviour
+{
+    private GameObject Player;
+
+    public void SetPlayer(GameObject player)
+    {
+        Player = player;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.root.gameObject != Player && other.gameObject.CompareTag("Player"))
+        {
+            other.transform.root.GetComponent<Rigidbody>().AddExplosionForce(100000, gameObject.transform.position, 10f, 10, ForceMode.Force);
+        }
+    }
+}
