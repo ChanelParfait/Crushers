9aa8d20e1eed8ede1a5e03affe0d005ef897408e
Kaytolover123 <67986731+ChanelParfait@users.noreply.github.com>
2024-09-24 19:25:56 +1000

    Restart button added to leaderboard scene

--- Crushers/Assets/PickUp/PickUpManager.cs
+++ Crushers/Assets/PickUp/PickUpManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
