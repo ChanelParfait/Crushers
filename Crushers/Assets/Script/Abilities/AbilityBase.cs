63c0a78e984e792d1be4e7ed8675a612422ae92d
Iskandar <iskandarmuminov@mail.ru>
2024-12-06 17:28:42 +1100

    GM-177

--- Crushers/Assets/Script/Abilities/AbilityBase.cs
+++ Crushers/Assets/Script/Abilities/AbilityBase.cs
@@ -4,12 +4,22 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
+public enum AttachmentPos
+{
+    Back,
+    Bottom,
+    Left,
+    Right,
+    Top,
+    Front
+}
+
 public abstract class AbilityBase : ScriptableObject
 {
     [Header("Ability Info")]
     [SerializeField] string title;
     [SerializeField] public Sprite icon;
-
+    public AttachmentPos attachmentPos;
     public abstract void Use(GameObject controlledCar); 
 
 
