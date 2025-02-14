735299cfe0b3e257cda63036a1318df92a15b586
ARARECREATURE <111717881+ARARECREATURE@users.noreply.github.com>
2025-02-14 11:31:32 +1100

    Fixed?

--- Crushers/Assets/Script/UI/VehicleUIController.cs
+++ Crushers/Assets/Script/UI/VehicleUIController.cs
@@ -11,7 +11,7 @@ using Image = UnityEngine.UI.Image;
 
 public class VehicleUIController : MonoBehaviour
 {
-   
+
 
     [SerializeField] private TextMeshProUGUI levelTimerTxt;
     public GameObject startTimer;
@@ -24,7 +24,7 @@ public class VehicleUIController : MonoBehaviour
     private ScoreKeeper _scoreKeeper;
 
     [SerializeField] private GameObject killFeed;
-    
+
     [SerializeField] private Transform killfeedSpawn;
 
     [SerializeField] private Sprite[] allDeathTypeSprites;
@@ -33,33 +33,38 @@ public class VehicleUIController : MonoBehaviour
 
     void OnEnable()
     {
-        LevelManager.levelTimeChanged +=  UpdateLevelTimer;
-        LevelManager.startTimeChanged +=  UpdateStartTimer;
-        
-        
+        LevelManager.levelTimeChanged += UpdateLevelTimer;
+        LevelManager.startTimeChanged += UpdateStartTimer;
+
+
         AbilityManager.OnAbilityUse.AddListener(StartCooldownUI);
-        
-      
+
+        ScoreKeeper.OnGlobalPlayerScored.AddListener(ShowKillFeed);
+
     }
 
     void OnDisable()
     {
-        LevelManager.levelTimeChanged -=  UpdateLevelTimer;
-        LevelManager.startTimeChanged -=  UpdateStartTimer;
+        LevelManager.levelTimeChanged -= UpdateLevelTimer;
+        LevelManager.startTimeChanged -= UpdateStartTimer;
 
         AbilityManager.OnAbilityUse.RemoveListener(StartCooldownUI);
-        
-       
+
+        ScoreKeeper.OnGlobalPlayerScored.RemoveListener(ShowKillFeed);
     }
 
     private void Start()
     {
         abilityManager = gameObject.GetComponentInParent<AbilityManager>();
-        if(abilityManager){
+        if (abilityManager)
+        {
             abilityResetSlider.maxValue = abilityManager.GetAbilityCooldownTime();
             abilityResetSlider.value = abilityResetSlider.maxValue;
         }
-        
+
+
+
+
     }
 
     private void StartCooldownUI(float cooldownTime)
@@ -68,28 +73,32 @@ public class VehicleUIController : MonoBehaviour
         {
             StopCoroutine(cooldownRoutine);
         }
+
         cooldownRoutine = StartCoroutine(CooldownUI(cooldownTime));
     }
 
     private IEnumerator CooldownUI(float cooldownTime)
     {
         float elapsedTime = 0f;
-        abilityResetSlider.value = 0; 
+        abilityResetSlider.value = 0;
 
         while (elapsedTime < cooldownTime)
         {
             elapsedTime += Time.deltaTime;
-            abilityResetSlider.value = Mathf.Min(abilityResetSlider.maxValue, elapsedTime / cooldownTime * abilityResetSlider.maxValue);
+            abilityResetSlider.value = Mathf.Min(abilityResetSlider.maxValue,
+                elapsedTime / cooldownTime * abilityResetSlider.maxValue);
             yield return null;
         }
 
-        abilityResetSlider.value = abilityResetSlider.maxValue; 
+        abilityResetSlider.value = abilityResetSlider.maxValue;
     }
 
-    public void UpdateLevelTimer(int time){
-        if(time <= 10){
+    public void UpdateLevelTimer(int time)
+    {
+        if (time <= 10)
+        {
             StartCountdown(time);
-            return; 
+            return;
         }
 
         // convert time to minutes and seconds
@@ -99,26 +108,32 @@ public class VehicleUIController : MonoBehaviour
         levelTimerTxt.SetText(string.Format("{0:0}:{1:00}", minutes, seconds));
     }
 
-    public void UpdateStartTimer(int time){
+    public void UpdateStartTimer(int time)
+    {
 
-        if(time == 0){
+        if (time == 0)
+        {
             // set text to "GO"
             startTimerTxt.SetText("GO!");
             startTimerTxt.color = Color.green;
             StartCoroutine(ClearStartTimer());
-        } else {
+        }
+        else
+        {
             startTimerTxt.SetText(time.ToString());
         }
     }
 
-    private void StartCountdown(int time){
+    private void StartCountdown(int time)
+    {
         levelTimerTxt.gameObject.SetActive(false);
         startTimerTxt.color = Color.red;
         startTimerTxt.SetText(time.ToString());
         startTimer.SetActive(true);
     }
 
-    private IEnumerator ClearStartTimer(){
+    private IEnumerator ClearStartTimer()
+    {
         yield return new WaitForSeconds(1);
         startTimer.SetActive(false);
 
@@ -128,11 +143,13 @@ public class VehicleUIController : MonoBehaviour
     {
         Debug.Log("Showing kill feed");
         GameObject k = Instantiate(killFeed, killfeedSpawn.position, killfeedSpawn.rotation);
-        k.transform.SetParent(killfeedSpawn);
+        k.transform.SetParent(killfeedSpawn, false);
+        k.transform.localPosition = Vector3.zero;
+        k.transform.localRotation = Quaternion.identity;
 
         string scoringPlayerLayerName = LayerMask.LayerToName(ScoringPlayer.layer);
         string defeatedPlayerLayerName = LayerMask.LayerToName(DefeatedPlayer.layer);
-        
+
         foreach (Transform child in k.transform)
         {
             if (child.name == "PlayerScored")
@@ -140,20 +157,22 @@ public class VehicleUIController : MonoBehaviour
                 TextMeshProUGUI scoringPlayerText = child.GetComponent<TextMeshProUGUI>();
                 scoringPlayerText.text = scoringPlayerLayerName;
             }
+
             if (child.name == "DefeatedPlayer")
             {
                 TextMeshProUGUI defeatedPlayerText = child.GetComponent<TextMeshProUGUI>();
                 defeatedPlayerText.text = defeatedPlayerLayerName;
             }
+
             if (child.name == "DeathType")
             {
                 Image deathType = child.GetComponent<Image>();
-                deathType.sprite =  ReturnDeathSprite(Death);
+                deathType.sprite = ReturnDeathSprite(Death);
             }
         }
-        
-        Destroy(k, 2f);
-        
+
+        Destroy(k, 5f);
+
     }
 
     private Sprite ReturnDeathSprite(TypeOfDeath Death)
@@ -168,20 +187,16 @@ public class VehicleUIController : MonoBehaviour
                 ReturnedSprite = allDeathTypeSprites[1];
                 break;
             case TypeOfDeath.Rocket:
-                ReturnedSprite =  allDeathTypeSprites[2];
+                ReturnedSprite = allDeathTypeSprites[2];
                 break;
             case TypeOfDeath.Spike:
-                ReturnedSprite =  allDeathTypeSprites[3];
+                ReturnedSprite = allDeathTypeSprites[3];
                 break;
             case TypeOfDeath.Void:
-                ReturnedSprite =  allDeathTypeSprites[4];
+                ReturnedSprite = allDeathTypeSprites[4];
                 break;
         }
 
         return ReturnedSprite;
     }
-
-    
-
-
 }
